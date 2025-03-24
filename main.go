d777154e7c6de5c1bc1e943df1f0e5d3bc28e675
kly777 <kly777777777@gmail.com>
2025-03-25 00:01:59 +0800

    略

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	defer cleanup()
 
 	fmt.Println(time.Now().Format("2006/01/02 15:04:05"), "Server is running on port 8080")
-	fmt.Println("\n\n\nGREAT!!!!!")
+	fmt.Println("\nGREAT!!!!!")
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
